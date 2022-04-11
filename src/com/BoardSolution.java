dbe68d4ed0a72db36cb7e15e9ee7bbd7ad635687
R1con <igor.prilutzkij@yandex.ru>
2022-04-11 10:56:19 +0300

    init

--- src/com/BoardSolution.java
+++ src/com/BoardSolution.java
@@ -1,5 +1,8 @@
 package com;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class BoardSolution {
 
     public static boolean checkValidNumber(int[] array) {
@@ -20,18 +23,52 @@ public class BoardSolution {
         return true;
     }
 
-    public static boolean isNotDuplicateCharOnRow(char[][] board) {
+    public static boolean checkValidNumber1(int[] array, char[] rows) {
+        for (int i = 0; i < array.length; i++) {
+            if (rows[i] == '.') {
+                continue;
+            }else if (rows[i] >= '1' || rows[i] <= '9') {
+                array[Character.digit(rows[i], 10) - 1] = Character.digit(rows[i], 10);
+                for (int j = 0; j < array.length; j++) {
 
-        for (int i = 0; i < board.length; i++) {
-            int[] arrayRows = new int[9];
-            for (int j = 0; j < board[i].length; j++) {
-                arrayRows[j] = Character.getNumericValue(board[i][j]);
-            }
+                }
 
-            if(!checkValidNumber(arrayRows)) {
+            } else {
                 return false;
             }
         }
+
+        return true;
+    }
+
+    public static boolean checkValidNumberWithHashSet() {
+
+        return true;
+    }
+
+
+
+
+
+    public static boolean isNotDuplicateCharOnRow(char[][] board) {
+
+
+        for (int i = 0; i < board.length; i++) {
+            int[] arrayRows = new int[9];
+            char[] rows = board[i];
+            if (!checkValidNumber1(arrayRows, rows))
+                    return false;
+//            for (int j = 0; j < board[i].length; j++) {
+//                if (!checkValidNumber1(arrayRows, rows))
+//                    return false;
+//                //arrayRows[j] = Character.getNumericValue(board[i][j]);
+//            }
+
+//            if (!checkValidNumber(arrayRows)) {
+//                return false;
+//            }
+        }
+
 
         return true; // дубликата в строке нет
     }
@@ -46,6 +83,25 @@ public class BoardSolution {
 
             if(!checkValidNumber(arrayCols)) {
                 return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean isValidBoardWithHashSet(char[][] board) {
+
+        Set<String> validElementSet = new HashSet<>();
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] == '.') {
+                    continue;
+                } else if (board[i][j] >= '1' && board[i][j] <= '9') {
+                    if (!validElementSet.add("row " + board[i][j] + i) || !validElementSet.add("col" + board[i][j] + j))
+                        return false;
+                } else {
+                    return false;
+                }
             }
         }
 
@@ -89,7 +145,7 @@ public class BoardSolution {
                 {'.', '.', '.', '.', '8', '.', '.', '7', '9'}};
 
         char[][] board2 = {
-                {'5', 'а', '.', '.', '7', '.', '.', '.', '.'},
+                {'.', '.', '.', '.', '7', '.', '5', '.', '.'},
                 {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
                 {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
                 {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
@@ -99,6 +155,6 @@ public class BoardSolution {
                 {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
                 {'.', '.', '.', '.', '8', '.', '.', '7', '9'}};
 
-        System.out.println(isValidBoard(board2));
+        System.out.println(isValidBoardWithHashSet(board2));
     }
 }
