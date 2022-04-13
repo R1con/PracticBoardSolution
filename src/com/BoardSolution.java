c9d91e88229670651f83b6573513b4975ed7d543
R1con <igor.prilutzkij@yandex.ru>
2022-04-13 11:45:37 +0300

    init

--- src/com/BoardSolution.java
+++ src/com/BoardSolution.java
@@ -5,79 +5,24 @@ import java.util.Set;
 
 class BoardSolution {
 
-    private boolean isNotDuplicateOnRow(char[][] board) {
-
-        for (int i = 0; i < board.length; i++) {
-            boolean[] setElementRow = new boolean[9];
-            for (int j = 0; j < board[i].length; j++) {
-                if (board[i][j] >= '1' && board[i][j] <= '9') {
-                    if (setElementRow[Character.digit(board[i][j], 10) - 1]) {
-                        System.out.println("Duplicate on row = " + (i + 1) + ", element = " + board[i][j]);
-                        return false;
-                    } else {
-                        setElementRow[Character.digit(board[i][j], 10) - 1] = true;
-                    }
-                } else if (board[i][j] != '.') {
-                    System.out.println("Not valid symbol = " + board[i][j] + ", in row = " + (i + 1));
-                    return false;
-                }
-            }
-        }
-
-        return true;
-    }
-
-    private boolean isNotDuplicateOnCol(char[][] board) {
-
-        for (int j = 0; j < board.length; j++) {
-            boolean[] setElementCol = new boolean[9];
-            for (int i = 0; i < board[j].length; i++) {
-                if (board[i][j] >= '1' && board[i][j] <= '9') {
-                    if (setElementCol[Character.digit(board[i][j], 10) - 1]) {
-                        System.out.println("Duplicate on col = " + (j + 1) + ", element = " + board[i][j]);
-                        return false;
-                    } else {
-                        setElementCol[Character.digit(board[i][j], 10) - 1] = true;
-                    }
-                } else if (board[i][j] != '.') {
-                    System.out.println("Not valid symbol = " + board[i][j] + ", in col = " + j);
-                    return false;
-                }
-            }
-        }
-
-        return true;
-    }
-
     public boolean isValidBoardWithHashSet(char[][] board) {
 
         for (int row = 0; row < board.length; row++) {
-            Set<Integer> colSet = new HashSet<>();
-            Set<Integer> rowSet = new HashSet<>();
+            Set<Character> colSet = new HashSet<>();
+            Set<Character> rowSet = new HashSet<>();
             for (int column = 0; column < board[row].length; column++) {
-                if (board[column][row] != '.' && !colSet.add(Character.digit(board[column][row], 10))
-                        || board[row][column] != '.' && !rowSet.add(Character.digit(board[row][column], 10))) {
-                    System.out.print("Matrix is not valid: ");
+                if (board[row][column] != '.' && board[row][column] < '1' | board[row][column] > '9' | !rowSet.add(board[row][column])) {
+                    System.out.print("Matrix is not valid");
                     return false;
                 }
-
+                if (board[column][row] != '.' && board[column][row] < '1' | board[column][row] > '9' | !colSet.add(board[column][row])) {
+                    System.out.print("Matrix is not valid");
+                    return false;
+                }
             }
         }
 
-        System.out.print("Matrix is  valid: ");
-        return true;
-    }
-
-    public boolean isValidBoardWithArray(char[][] board) {
-
-        boolean flag;
-        if (!isNotDuplicateOnRow(board) || !isNotDuplicateOnCol(board)) {
-            System.out.print("Matrix is  not valid:" + " ");
-            flag = false;
-            return flag;
-        }
-
-        System.out.print("Matrix is  valid:" + " ");
+        System.out.print("Matrix is  valid");
         return true;
     }
 }
